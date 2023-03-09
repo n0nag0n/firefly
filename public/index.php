29d850b5b21a9944251bd1d2ad95cc7c3e9ecea2
n0nag0n <n0nag0n@sky-9.com>
2023-03-08 18:08:24 -0700

    made bootstrap file local and tweaked index file

--- public/index.php
+++ public/index.php
@@ -1,48 +1,58 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+	<!-- Bootstrap CSS -->
+	<link href="/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>firefly</title>
+	<title>firefly</title>
 </head>
 <body class="bg-light">
 <div class="container bg-white p-3">
-    <div class="starter-template mt-3">
+	<div class="starter-template mt-3">
 
-        <h1>Welcome to firefly 1.0.0!</h1>
-        <p class="lead">firefly is a developer box where you can switch PHP versions easily.</p>
+		<h1>Welcome to Firefly 1.0.0!</h1>
+		<p class="lead">Firefly is a developer box where you can switch PHP versions easily.</p>
 
-        <h3>Preconfigured Hosts</h3>
-        <p>
-            please edit your local hosts file to use this! <br><br>
-            <strong>PHP 8.2: </strong><a href="http://firefly82.com">http://firefly82.com</a><br>
-			<strong>PHP 8.1: </strong><a href="http://firefly81.com">http://firefly81.com</a><br>
-            <strong>PHP 8.0: </strong><a href="http://firefly80.com">http://firefly80.com</a><br>
-            <strong>PHP 7.4: </strong><a href="http://firefly74.com">http://firefly74.com</a><br>
-            <strong>PHP 7.3: </strong><a href="http://firefly73.com">http://firefly73.com</a><br>
-            <strong>PHP 7.2: </strong><a href="http://firefly72.com">http://firefly72.com</a><br>
-            <strong>PHP 7.1: </strong> <a href="http://firefly71.com">http://firefly71.com</a><br>
-            <strong>PHP 7.0: </strong> <a href="http://firefly70.com">http://firefly70.com</a><br>
-            <strong>PHP 5.6: </strong> <a href="http://firefly56.com">http://firefly56.com</a><br>
-            <br>
-            <strong>show PHP info: </strong> <a href="info.php">info.php</a><br>
-        </p><br>
+		<p class="fst-italic">Current PHP Version: <?=phpversion()?></p>
 
-        <h3>Custom Hosts</h3>
-        <p>
-            you can use our provisioning templates (directory: provisioning) to add your custom projects
-        </p>
+		<h3>Preconfigured Hosts</h3>
+		<p>
+			please edit your local hosts file to use this! <br><br>
+			<strong>PHP 8.2: </strong><a href="http://firefly82.com">http://firefly82.com</a>: <a href="http://firefly82.com/info.php">info.php</a><br>
+			<strong>PHP 8.1: </strong><a href="http://firefly81.com">http://firefly81.com</a>: <a href="http://firefly81.com/info.php">info.php</a><br>
+			<strong>PHP 8.0: </strong><a href="http://firefly80.com">http://firefly80.com</a>: <a href="http://firefly80.com/info.php">info.php</a><br>
+			<strong>PHP 7.4: </strong><a href="http://firefly74.com">http://firefly74.com</a>: <a href="http://firefly74.com/info.php">info.php</a><br>
+			<strong>PHP 7.3: </strong><a href="http://firefly73.com">http://firefly73.com</a>: <a href="http://firefly73.com/info.php">info.php</a><br>
+			<strong>PHP 7.2: </strong><a href="http://firefly72.com">http://firefly72.com</a>: <a href="http://firefly72.com/info.php">info.php</a><br>
+			<strong>PHP 7.1: </strong> <a href="http://firefly71.com">http://firefly71.com</a>: <a href="http://firefly71.com/info.php">info.php</a><br>
+			<strong>PHP 7.0: </strong> <a href="http://firefly70.com">http://firefly70.com</a>: <a href="http://firefly70.com/info.php">info.php</a><br>
+			<strong>PHP 5.6: </strong> <a href="http://firefly56.com">http://firefly56.com</a>: <a href="http://firefly56.com/info.php">info.php</a><br>
+		</p>
 
-        <h3>Docs</h3>
-        <p>
-            <a href="https://github.com/n0nag0n/firefly" target="_blank">https://github.com/n0nag0n/firefly</a>
-        </p>
-    </div>
+		<h3>Database Access</h3>
+		<p>Database access via a UI is provided by <a href="https://adminer.org" target="_blank">Adminer</a>.</p>
+
+		<p><a href="/adminer.php" target="_blank">Database Access</a></p>
+
+		<h3>Beanstalkd Access</h3>
+		<p>Beanstalkd tube access via a UI is provided by <a href="https://github.com/ptrofimov/beanstalk_console" target="_blank">beanstalk_console</a>.</p>
+
+		<p><a href="/beanstalkd" target="_blank">Beanstalkd Access</a></p>
+
+		<h3>Custom Hosts</h3>
+		<p>
+			You can use our provisioning templates (directory: provisioning) to add your custom projects
+		</p>
+
+		<h3>Docs</h3>
+		<p>
+			See README file in <a href="https://github.com/n0nag0n/firefly" target="_blank">https://github.com/n0nag0n/firefly</a>
+		</p>
+	</div>
 
 </div>
 </body>
